f5b71bacc997cc1815c4c6084cf47e00302ec71e
roughdraftofme <45472736+roughdraftofme@users.noreply.github.com>
2021-02-25 15:17:52 -0500

    asset dump + enemy spawning & movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.LookAt(target.transform);
+        transform.position = transform.position + (transform.forward * speed * Time.smoothDeltaTime);
+    }
+}
---
+++ Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject[] enemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    int index;
+
+
+    void Start()
+    {
+        StartCoroutine (EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(-300, -230);
+            zPos = Random.Range(110, 180);
+            index = Random.Range(0, 5);
+
+            Vector3 spawn = new Vector3(xPos, 40, zPos);
+
+            Instantiate(enemy[index], spawn, Quaternion.identity);
+            if (index == 1)
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    Instantiate(enemy[index], spawn, Quaternion.identity);
+                    yield return new WaitForSeconds(0.5f);
+                }
+            }
+
+            yield return new WaitForSeconds(5);
+            enemyCount++;
+        }
+    }
+    
+}
